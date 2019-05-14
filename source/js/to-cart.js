8ad2b6b108a7a60d6bc13fabf364b40b968c5d7a
GulnaraY <guga.ufa@gmail.com>
2019-05-15 01:44:29 +0300

    Дополнения по фокусу в модальном

--- source/js/to-cart.js
+++ source/js/to-cart.js
@@ -1,22 +1,29 @@
 var orderButtons = document.querySelectorAll('.order-button');
 var cartPopup = document.querySelector('.to-cart');
 var closePopup = document.querySelector('.to-cart__button');
-var overlay = document.querySelector(".modal-overlay");
-var sizeLabel = document.querySelector("[id=size-s]");
+var overlay = document.querySelector('.modal-overlay');
+var sizeLabel = document.querySelector('[checked]');
+var sizeField = document.querySelectorAll('.to-cart__size');
+var form = cartPopup.querySelector('.to-cart__form');
+var currentPosition = sizeLabel;
 
 for (var j=0; j<=orderButtons.length-1; j++) {
   orderButtons[j].addEventListener('click', function(evt){
-  evt.preventDefault();
-  if (!cartPopup.classList.contains('to-cart--opened')){
-    cartPopup.classList.add('to-cart--opened');
-    overlay.classList.add('modal-overlay--opened');
-    sizeLabel.focus();
-  }
-});
+    evt.preventDefault();
+    if (!cartPopup.classList.contains('to-cart--opened')){
+      cartPopup.classList.add('to-cart--opened');
+      overlay.classList.add('modal-overlay--opened');
+      currentPosition.focus();
+    }
+  });
+}
+for (var j=0; j<=sizeField.length-1; j++) {
+  sizeField[j].addEventListener('click', function(evt){
+  currentPosition = evt.target;
+  });
 }
 
-closePopup.addEventListener('click', function(evt){
-  evt.preventDefault();
+form.addEventListener('submit', function(evt){
   if (cartPopup.classList.contains('to-cart--opened')){
     cartPopup.classList.remove('to-cart--opened');
     overlay.classList.remove('modal-overlay--opened');
@@ -26,9 +33,10 @@ closePopup.addEventListener('click', function(evt){
 window.addEventListener("keydown", function(evt){
   if(evt.keyCode === 27){
     evt.preventDefault();
-    if(cartPopup.classList.contains("to-cart--opened")){
-      cartPopup.classList.remove("to-cart--opened");
+    if(cartPopup.classList.contains('to-cart--opened')){
+      cartPopup.classList.remove('to-cart--opened');
       overlay.classList.remove('modal-overlay--opened');
+
     }
   }
 });
